196b7a55e2c9a459f61b58645d258d571c0feb80
Beinsezii <beinsezii@gmail.com>
2023-10-05 21:50:27 -0700

    Fix CH texture

--- src/gui/lch.rs
+++ src/gui/lch.rs
@@ -80,16 +80,16 @@ impl<'a> Widget for LCH<'a> {
                             .map(|c| {
                                 (0..72)
                                     .map(|h| {
-                                        let mut p = [self.value[0], c as f32, h as f32 * 5.0];
+                                        let mut p = [self.value[0], (100 - c) as f32, h as f32 * 5.0];
                                         convert_space(Space::LCH, Space::LRGB, &mut p);
                                         Rgba::from_rgb(p[0], p[1], p[2]).into()
                                     })
                                     .collect::<Vec<Color32>>()
                             })
-                            .fold(Vec::with_capacity(3 * 72 * 101), |mut acc, mut e| {
+                            .reduce(|mut acc, mut e| {
                                 acc.append(&mut e);
                                 acc
-                            }),
+                            }).unwrap(),
                     };
                     let chtexture = ui.ctx().load_texture(
                         format!("{} CH", self.text),
